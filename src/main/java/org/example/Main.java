f3f26426f403fcb42f29924bc0fa957f24fcaf10
Maidanov <trofim.maidanov@getexperts.loc>
2023-09-20 18:26:34 +0300

    testDriveGPT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,43 @@
 package org.example;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         FileInputStream file = null;
         try {
-            file = new FileInputStream("путь_к_исходному_файлу.docx");
-            XWPFDocument doc = new XWPFDocument(file);
+            file = new FileInputStream("C:\\Users\\maidanovt\\Desktop\\AutoResume\\Get experts_Кочетков Алексей.docx");
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        XWPFDocument doc = new XWPFDocument(file);
+        XWPFParagraph paragraph = doc.getParagraphs().get(5); // получение первого параграфа
+        String text = paragraph.getText();
 
+        XWPFDocument newDoc = new XWPFDocument();
+
+        XWPFParagraph newParagraph = newDoc.createParagraph(); // создание нового параграфа
+        XWPFRun run = newParagraph.createRun(); // создание нового текстового блока в параграфе
+        run.setText(text); // установка текста
+
+        run.setFontSize(12);
+        run.setFontFamily("Arial");
+        run.setBold(true);
+
+        run.setItalic(true);
+        run.setUnderline(UnderlinePatterns.SINGLE);
+
+        newParagraph.setAlignment(ParagraphAlignment.CENTER);
+        FileOutputStream out = new FileOutputStream("C:\\Users\\maidanovt\\Desktop\\AutoResume\\new.docx");
+        newDoc.write(out);
+        out.close();
     }
 }
